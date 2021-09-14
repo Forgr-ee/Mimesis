0a8d62285d8ab664d9843af3d8f02dce96606695
martindonadieu <martindonadieu@gmail.com>
2021-09-14 13:31:21 +0300

    fix: crisp id

--- src/services/crips.ts
+++ src/services/crips.ts
@@ -31,7 +31,7 @@ export const initCrisp = (): void => {
     } else {
       window.$crisp = []
     }
-    window.CRISP_WEBSITE_ID = '1f5d5a70-2622-4536-a454-996394feeaad'
+    window.CRISP_WEBSITE_ID = '1011b75e-c4f6-400c-a6ff-c5077adb9db3'
     const s = document.createElement('script')
     s.src = 'https://client.crisp.chat/l.js'
     s.async = true
